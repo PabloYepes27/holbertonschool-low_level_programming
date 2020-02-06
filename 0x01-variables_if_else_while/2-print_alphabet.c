90e48e02677ff93267706124335f1d67f9a83cb7
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-06 14:58:20 +0000

    Creating the folder and adding the problems 0, 1 and 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
