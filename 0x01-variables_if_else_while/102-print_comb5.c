75e1b9965fbc430d5627f90f8d0389e965d074ae
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-07 00:06:27 +0000

    Problem 13

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int w, x, y, z;
+
+	for (w = '0' ; w <= '9'; w++)
+	{
+		for (x = '0'; x <= '8'; x++)
+		{
+			for (y = '0'; y <= '9'; y++)
+			{
+				for (z = y + 1; z <= '9'; z++)
+				{
+					putchar(w);
+					putchar(x);
+					putchar(' ');
+					putchar(y);
+					putchar(z);
+					if ((w != '9') || (x != '8') || (y != '9') || (z != '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
