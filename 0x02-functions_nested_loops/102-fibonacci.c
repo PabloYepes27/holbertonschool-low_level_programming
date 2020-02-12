1dbea64eb011ccc1e7436a0d7a2e16384ed6d634
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-12 21:23:38 +0000

    Problem 14 V.2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,24 +7,21 @@
 
 int main(void)
 {
-	float x = 0, y = 1, z = 1;
+	unsigned long x = 0, y = 1, z = 1;
 	int i, num = 50;
 
 	for (i = 1; i <= num; i++)
 	{
+		z = x + y;
+		x = y;
+		y = z;
 		if (i != num)
 		{
-			z = x + y;
-			x = y;
-			y = z;
-			printf("%.f, ", z);
+			printf("%lu, ", z);
 		}
 		else
 		{
-			z = x + y;
-			x = y;
-			y = z;
-			printf("%.f", z);
+			printf("%lu", z);
 		}
 	}
 	printf("\n");
