3c938bc9935671e15955e9e5d5d64e45efcaf5f3
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-12 20:45:12 +0000

    problem 12 V.2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,54 +1,54 @@
 #include "holberton.h"
+
 /**
-* print_times_table - function that prints the 9 times table, starting with 0.
+* times_table - function that prints the 9 times table, starting with 0.
 * Return:
 */
+
 void print_times_table(int n)
 {
-	int x, o, j = 0;
-
+	int o, p, j = 0;
+	
 	if ((n >= 0) && (n <= 15))
 	{
-		for (x = 0; x <= n; x++)
+		for (p = 0; p <= n; p++)
 		{
 			for (o = 0; o <= n; o++)
 			{
-				j = o * x;
-				if (o != 0)
+				j = o * p;
+				if (j <= 9)
 				{
-					if (j <= 9)
+					if (o != 0)
 					{
 						_putchar(',');
 						_putchar(' ');
 						_putchar(' ');
 						_putchar(' ');
-						_putchar((j/100) + '0');
-						_putchar(((j % 100) / 10) + '0');
-						_putchar(((j % 100) % 10) + '0');
-					}
-					else
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar((j / 10) + '0');
-						_putchar((j % 10) + '0');
-					}
-				}
-				else
-				{
-					if (j < n)
-					{
 						_putchar(j + '0');
 					}
 					else
 					{
-						_putchar((j / 10) + '0');
-						_putchar((j % 10) + '0');
+						_putchar(j + '0');
 					}
 				}
+				else if (j < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((j / 10) + '0');
+					_putchar((j % 10) + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((j / 100) + '0');
+					_putchar(((j % 100) / 10) + '0');
+					_putchar(((j % 100) % 10) + '0');
+				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
-
