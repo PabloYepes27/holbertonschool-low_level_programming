e881fc8ef21f19a0d4c29919d7496285c585e1b4
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-12 21:40:04 +0000

    Problem 15 V.2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	float x = 0, y = 1, z = 1, s = 0;
+	unsigned long x = 0, y = 1, z = 1, s = 0;
 	int i, num = 32;
 
 	for (i = 1; i <= num; i++)
@@ -15,8 +15,11 @@ int main(void)
 		z = x + y;
 		x = y;
 		y = z;
+		if ((z < 4000000) && (z % 2 == 0))
+		{
 		s = z + s;
+		}
 	}
-	printf("%.f\n", s);
+	printf("%lu\n", s);
 	return (0);
 }
