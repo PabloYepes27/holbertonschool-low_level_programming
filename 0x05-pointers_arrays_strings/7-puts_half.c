1177add52500d68f9286ba0762a80e4a93aeab8c
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-18 21:01:09 +0000

    Problem 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,7 +16,7 @@ void puts_half(char *str)
 	}
 	if ((i % 2) != 0)
 	{
-		for (j = ((i - 1) / 2); str[j] != '\0'; j++)
+		for (j = ((i + 1) / 2); str[j] != '\0'; j++)
 		{
 			_putchar(str[j]);
 		}
