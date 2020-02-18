1ae2d75fcad4ad9ebeca0379434950807dfcc22b
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-18 20:06:38 +0000

    Problem 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints other character of a string, starting with first character
+ * @str: string
+ * Return:
+ **/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
