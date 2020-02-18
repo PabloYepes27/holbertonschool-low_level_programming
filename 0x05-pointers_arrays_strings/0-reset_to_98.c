a4c73af74c2eb1a227f4e8985a4f52d9778afe74
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-18 17:38:42 +0000

    Problem 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 -  function that updates the value it points to to 98
+ * @n: pointer
+ * Return: Not expected.
+**/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
