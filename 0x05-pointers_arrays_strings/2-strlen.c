58dba91475cbdac6fc4696fdafaf7d8ee2a62325
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-18 18:11:22 +0000

    Problem 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: pointer of type char
+ * Return: Length of the string
+**/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
