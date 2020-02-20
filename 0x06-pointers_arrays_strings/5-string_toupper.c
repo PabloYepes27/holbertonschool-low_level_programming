144209e9bb1186a43d3973628e04d158de23dd06
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-20 23:38:24 +0000

    Problem 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  * *string_toupper - function that changes all lowercase to uppercase
+  * @*: string
+  * Return: string to uppercase
+**/
+
+char *string_toupper(char *s)
+{
+	int x;
+
+	for (x = 0; s[x]; x++)
+	{
+		if((s[x] >= 97) && (s[x] <= 122))
+		{
+			s[x] = (s[x] - 32);
+		}
+	}
+	return (s);
+}
