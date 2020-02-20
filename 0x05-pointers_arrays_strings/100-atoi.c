918a911abc2c317a855d9c957f61686a5da25f4d
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-20 03:05:40 +0000

    Problem 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+  * _atoi - function that convert a string to an integer
+  * @s: string
+  * Return: integer
+**/
+
+int _atoi(char *s)
+{
+	int i, sign = 1, n = 0;
+
+	for (i = 0; s[i]; ++i)
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		if ((s[i] >= '0') && (s[i] <= '9'))
+		{
+			n *= 10;
+			n = s[i] - '0';
+		}
+		else
+		{
+			break;
+		}
+	}
+	n *= sign;
+	return (n);
+}
