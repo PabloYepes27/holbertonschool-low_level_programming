a4453987818f0f0db17fe67e7679f72a37315075
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-21 00:06:55 +0000

    Problem 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+  * *cap_string - function that capitalizes all words of a string
+  * @s: string
+  * Return: string to uppercase
+**/
+
+char *cap_string(char *s)
+{
+	int x;
+
+	for (x = 0; s[x]; x++)
+	{
+		if ((s[x] > 97) && (s[x] < 122)
+		&& ((s[x - 1] == ' ') || (s[x - 1] == '\n')
+		|| (s[x - 1] == ',') || (s[x - 1] == ';') || (s[x - 1] == '.')
+		|| (s[x - 1] == '!') || (s[x - 1] == '?') || (s[x - 1] == '(')
+		|| (s[x - 1] == ')') || (s[x - 1] == '{') || (s[x - 1] == '}')
+		|| (s[x - 1] == '\t')))
+		{
+			s[x] = (s[x] - 32);
+		}
+	}
+	return (s);
+}
