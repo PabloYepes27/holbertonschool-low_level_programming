60436b517dd4e732c1b134c386863988dadad938
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-24 17:26:57 +0000

    Problem 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+  * *_strchr - Function that locates a character in a string
+  * @s: String to change
+  * @c: char to locate
+  * Return: pointer to the first occurence of the character c
+**/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+
+		}
+	}
+	return ('\0');
+}
