f9325ddf102c07b3a84407d45ff58f85ba63fb62
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-24 21:12:15 +0000

    Problem 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+  * *_strpbrk - function that searches a string for any of a set of bytes.
+  * @s: string for compare
+  * @accept: string to compare
+  * Return: a pointer to the byte that mathces
+**/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (j = 0; s[j]; j++)
+	{
+		for  (i = 0; accept[i]; i++)
+		{
+			if (accept[i] == s[j])
+			{
+				return (s + j);
+			}
+		}
+	}
+	if (s[j] == '\0')
+	{
+		return (s + j);
+	}
+	return ('\0');
+
+}
