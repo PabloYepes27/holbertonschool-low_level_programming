df3a5dcd15ffa14ea90489fa9bba2e981148f21e
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-28 20:57:15 +0000

    Fixing betty style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/** main - Function that prints its name
+/**
+  * main - Function that prints its name
   * @argv: argument vector
   * @argc: argument counter
   * Return: 0
   **/
 
-int main (int __attribute__((unused)) argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
