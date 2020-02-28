7df270db73136832dbc9d163f60cfe6e509dc84c
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-28 22:33:59 +0000

    Problem 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+  * main - Function that adds positive numbers
+  * @argv: argument vector
+  * @argc: argument counter
+  * Return: 0
+  **/
+
+int main(int argc, char *argv[])
+{
+	int x, sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (x = 1; x < argc; x++)
+		{
+			if (isdigit(*argv[x]))
+			{
+				sum += atoi(argv[x]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
