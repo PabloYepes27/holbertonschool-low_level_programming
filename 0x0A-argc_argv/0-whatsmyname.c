8c4f7d3a55f8f2c512adf9622f1c9f260361e542
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-28 20:09:57 +0000

    Problem 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** main - Function that prints its name
+  * @argv: argument vector
+  * @argc: argument counter
+  * Return: 0
+  **/
+
+int main (int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
