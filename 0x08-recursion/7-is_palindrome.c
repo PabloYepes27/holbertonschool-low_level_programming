9989a2d360bf234b0368a2c66553c720577af98b
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-02-28 13:57:52 +0000

    Problem 7 - Cleaning code and comments

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
-int compare(int m, int i);
-int length(int s);
+int compare(char *q, int y, int len);
+int length(char *s);
 
 /**
   * is_palindrome - returns 1 if it is a palindrome, otherwise 0
-  * @n: number
+  * @s: string
   * Return: 1 or 0
   **/
 
@@ -14,49 +14,53 @@ int is_palindrome(char *s)
 {
 	int x = 0;
 	int len = length(s);
-	
-	return (compare(s, x, len));
+
+	if (*s != '\0')
+	{
+		return (compare(s, x, len - 1));
+	}
+	else
+		return (1);
 }
 
 /**
   * compare - Function that compare the string
-  * @m: number
-  * @i: root
+  * @q: string
+  * @y: initial position
+  * @len: final position
   * Return: 1 or 0
   **/
 
 int compare(char *q, int y, int len)
 {
-	if (s[y] < [len])
+	if (q[y] < q[len])
 	{
-		compare(q, (y + 1), (len - 1));
+		if (q[y] == q[len])
+		{
+			compare(q, (y + 1), (len - 1));
+		}
+		else
+		{
+			return (0);
+		}
 	}
-	else if (s[y] == s[len])
-	{ 
-		return (1);
-	}
-	else if (s[y] > s[len])
-	{
-		return (0);
-	}
+	return (1);
 }
 
 /**
   * length - Function that compare a string and take the length
-  * @s: number
-  * @x: root
+  * @s: string
   * Return: length of a string
   **/
 
 int length(char *s)
 {
 
-	if (s != '\0')
+	if (*s != '\0')
 	{
 		return (1 + length(s + 1));
 	}
-	else
-		return (-1);
+	return (0);
 }
 
 
