b2f3790810a45edb261afc503e1568d204c8ac4f
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-02 02:16:05 +0000

    Problem 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+
+int coins(int val);
 
 /**
   * main - Function that prints minimum number of coins to make a change
@@ -11,21 +12,57 @@
 
 int main(int argc, char *argv[])
 {
-	int x,value = 0, coins = 0;
+	int value;
 
-	value = atoi(argv[1]);
-
-	if (argc < 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	value = atoi(argv[1]);
 	if (value < 0)
 	{
 		printf("0\n");
 	}
-	if
-		printf("%d\n", coins);
+	else
+	{
+		printf("%d\n", coins(value));
 	}
 	return (0);
+}
+
+/**
+  * coins - Function that prints minimum number of coins to make a change
+  * @val: change
+  * Return: number of coins
+  **/
+
+
+int coins(int val)
+{
+	int a = 25, b = 10, c = 5, d = 2, e = 1, x;
+
+	for (x = 0; val != 0; x++)
+
+		if (val >= a)
+		{
+			val -= a;
+		}
+		else if (val >= b)
+		{
+			val -= b;
+		}
+		else if (val >= c)
+		{
+			val -= c;
+		}
+		else if (val >= d)
+		{
+			val -= d;
+		}
+		else
+		{
+			val -= e;
+		}
+	return (x);
 }
