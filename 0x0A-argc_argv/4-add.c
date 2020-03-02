ee6af87d66dad7df2e61b5d8eb8de2354e10b3bf
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-02 00:49:19 +0000

    Problem 4 - deleting else

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,22 +16,20 @@ int main(int argc, char *argv[])
 	if (argc < 2)
 	{
 		printf("0\n");
+		return (0);
 	}
-	else
+	for (x = 1; x < argc; x++)
 	{
-		for (x = 1; x < argc; x++)
+		if (isdigit(*argv[x]) != 0)
 		{
-			if (isdigit(*argv[x]) > 0)
-			{
-				sum += atoi(argv[x]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+			sum += atoi(argv[x]);
 		}
-		printf("%d\n", sum);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
+	printf("%d\n", sum);
 	return (0);
 }
