9f1584eddddfe2949a076d8f8375b968846b663a
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-05 19:12:09 +0000

    Problem 2 - creating auxiliar char pointer

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,12 +11,17 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
+	unsigned int i;
+	void *p;
+	char *auxp;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
+	auxp = p;
+	for (i = 0; i < nmemb; i++)
+		auxp[i] = '\0';
 	return (p);
 }
