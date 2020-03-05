44b4f90c7e86458b70d87f041006b5acaeaeaa97
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-05 18:18:06 +0000

    Problem 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+  * string_nconcat - function that concatenates two strings
+  * @s1: string 1
+  * @s2: string 2
+  * @n: characters of s2 to concatenate
+  * Return: string concatenated
+**/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, len1 = 0, len2 = 0;
+	char *p;
+
+	for (i = 0; s1[i]; i++)
+		len1++;
+	for (j = 0; s2[j]; j++)
+		len2++;
+	if (n >= j)
+	{
+		p = malloc(sizeof(char) * (len1 + len2));
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; i < len1; i++)
+			p[i] = s1[i];
+		for (j = 0; j < len2; j++)
+			p[i + j] = s2[j];
+		p[i + j] = '\0';
+	}
+	else
+	{
+		p = malloc(sizeof(char) * (len1 + n));
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; i < len1 ; i++)
+			p[i] = s1[i];
+		for (j = 0; j < n; j++)
+			p[i + j] = s2[j];
+		p[i + j] = '\0';
+	}
+	return (p);
+}
