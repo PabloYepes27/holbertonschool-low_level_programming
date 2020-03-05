bd783f343eb492fef2ca0ad7262cbb6dc13e1722
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-05 14:47:42 +0000

    Problem 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+  * malloc_checked - function that allocates memory
+  * @b: size
+  * Return: void pointer
+**/
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{	
+		exit (98);
+	}
+	return (p);
+}
