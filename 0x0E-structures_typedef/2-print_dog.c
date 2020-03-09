b59b1c146a222a2fdaab4284eee216ae7106dc71
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-09 21:44:16 +0000

    Problem 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * print_dog - Function that prints
+  * @d: my_dog
+  **/
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if ((*d).name == NULL)
+		{
+			(*d).name = "(nil)";
+		}
+		if ((*d).owner == NULL)
+		{
+			(*d).owner = "(nil)";
+		}
+		else
+		{
+			printf("Name: %s\n"
+				"Age: %f\n"
+				"Owner: %s\n", (*d).name, (*d).age, (*d).owner);
+		}
+
+	}
+}
