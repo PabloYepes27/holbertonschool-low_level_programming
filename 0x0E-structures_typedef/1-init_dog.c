6c956de8d0afbf99a37fe7dd149238e9fe0c75c2
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-09 21:04:59 +0000

    Problem 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+  * init_dog - Function that initialize a variable
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: dog's owner
+  * @d: my_dog
+  **/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	(*d).age = age;
+	d->owner = owner;
+}
