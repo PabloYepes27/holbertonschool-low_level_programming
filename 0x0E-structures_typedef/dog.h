692ce809dc1c9729b58e58ab6c791b6de38670d2
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-09 20:26:43 +0000

    Problem 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - info of a dog
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: Dog's owner
+  **/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
