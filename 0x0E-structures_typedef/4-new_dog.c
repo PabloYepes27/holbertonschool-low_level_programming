e3bc50611216077d793ec048da909f795f15ed34
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-10 23:15:31 +0000

    Problem 4 - creating space memory for members

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,20 +3,57 @@
 #include <stdio.h>
 
 /**
-  * print_dog - Function that prints
-  * @d: my_dog
+ * _strlen - function that returns the length of a string
+ * @s: pointer of type char
+ * Return: Length of the string
+**/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
+/**
+  * new_dog - function that creates a new dog
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: dog's owner
+  * Return: the new dog
   **/
 
-struct dog_t *new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog_t *my_dog;
+	dog_t *newdog;
+	int i;
 
-	my_dog = malloc(sizeof(dog_t));
-	if (my_dog == NULL)
+	newdog = malloc(sizeof(dog_t));
+	if (newdog == NULL)
 		return (NULL);
-	my_dog.name = name;
-	my_dog.age = age;
-	my_dog.owner = owner;
-	return (my_dog);
-
+	newdog->age = age;
+	newdog->name = malloc(_strlen(name) + 1);
+	if (newdog->name == NULL)
+	{
+		free(newdog);
+		return (NULL);
+	}
+	for (i = 0; name[i]; i++)
+		newdog->name[i] = name[i];
+	newdog->name[i] = '\0';
+	newdog->owner = malloc(_strlen(owner) + 1);
+	if (newdog->owner == NULL)
+	{
+		free(newdog);
+		return (NULL);
+	}
+	for (i = 0; owner[i]; i++)
+		newdog->owner[i] = owner[i];
+	newdog->owner[i] = '\0';
+	return (newdog);
 }
+
+
