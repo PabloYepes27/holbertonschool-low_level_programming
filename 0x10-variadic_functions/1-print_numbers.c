0d8fa1c19097e19d1e95e66b079ec3db9c33a0e6
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-14 18:40:12 +0000

    Problem 1 - modify condition for separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		va_start(lst, n);
 		for (i = 0; i < n; i++)
 		{
-			if (i != (n - 1))
+			if (i < (n - 1))
 			printf("%d%s", va_arg(lst, int), separator);
 			else
 			printf("%d", va_arg(lst, int));
