a627f26453536669fcc9c90c23615884f3f0f0c6
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-20 03:21:01 +0000

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+#include "stdio.h"
 
 void start(void) __attribute__ ((constructor));
 
@@ -9,6 +9,6 @@ void start(void) __attribute__ ((constructor));
 
 void start(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-	I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
