9c7f5d0d5f8e26e42da507aa757f2bb814ef3245
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-20 02:28:54 +0000

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - function that adds a new node at the end of a list_t list
+ * @head: pointer that point to the header node
+ * Return: The adress of the new.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	tmp = head;
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+	free(head);
+}
