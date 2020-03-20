1ae64d787437fb9a69223e32df8e245c121fcea2
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-20 02:32:26 +0000

    Task 4

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,23 +1,12 @@
 section .text
-                global _start
+        global main
 main:
-                mov ebx,1
-                mov ecx, msg
-                mov edx,17
-                mov eax,4
-                int 0x80
-                mov eax,0
-                int 0x80
+        mov ebx,1
+        mov ecx, msg
+        mov edx,17
+        mov eax,4
+        int 0x80
+        mov eax,0
+        int 0x80
 section .data
-                msg db  'Hello, Holberton',0xa
-                len equ $ - msg 
-section .text
-                _start:
-                mov edx,len
-                mov ecx,msg
-                mov ebx,1
-                mov eax,4
-                int 0x80
-                mov ebx,0
-                mov eax,1
-                int 0x80 
+        msg db  'Hello, Holberton',0xa
