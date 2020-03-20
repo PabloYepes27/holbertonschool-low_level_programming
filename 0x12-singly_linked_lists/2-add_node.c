a5508e6439100e4bf082d036265718b8ddef25d4
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-20 00:11:33 +0000

    Task 2 - allocate memory

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -27,13 +27,12 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
-	new = *head;
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
-	new->next = *head;
 	new->len = _strlen(str);
+	new->next = *head;
 	*head = new;
 	return (new);
 }
