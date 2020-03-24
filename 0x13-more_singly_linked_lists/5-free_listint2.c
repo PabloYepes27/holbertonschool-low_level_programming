1fa3ac041cf398173b4f7295e0aad813a965559b
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-24 16:05:13 +0000

    Task 5 - NULL condition

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (head == NULL)
+		return;
 	temp = *head;
-	if (*head = NULL)
-	{
-		return (NULL);
-	}
 	while (*head)
 	{
 		temp = *head;
