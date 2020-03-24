a4b2fbab9e9c065712ada7897ded5a2d6b46c75c
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-24 15:36:16 +0000

    Task 7 - condition inside loop

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,8 +15,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 	new = head;
-	for (i = 0; i < index; i++)
+	for (i = 0; new; i++)
 	{
+		if (i == index && new)
+			return (new);
 		new = new->next;
 	}
 	return (new);
