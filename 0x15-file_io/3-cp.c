119a155b45cf4433f105220577bcebca40511bd4
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-04-01 04:57:23 +0000

    Task 3 -V4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,9 +6,8 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-char *read_file(const char *filename, int count);
-int copy_file(const char *filename, char *text_content);
-int _strlen(char *s);
+void _close(int file);
+
 /**
   * main - Write a program that copies the content of a file to another file
   * @ac:  argument counter
@@ -18,94 +17,58 @@ int _strlen(char *s);
 
 int main(int ac, char **av)
 {
-	char *file_from;
+	int fd1, fd2, rd, wr;
+	char buf[1024];
 
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	*file_from = read_file(av[1], 1024);
-	copy_file(av[2], file_from);
+	fd1 = open(av[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	do {
+		rd = read(fd1, buf, 1024);
+		if (rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+		wr = write(fd2, buf, rd);
+		if (wr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	} while (rd == 1024);
+	_close(fd1);
+	_close(fd2);
 	return (0);
 }
 
 /**
-  * read_file - program that reads the content of a file
-  * @filename:  name of the file to copy
-  * @count:  size of the buffer
-  * Return: 1 on success, -1 on failure
-**/
-
-char *read_file(const char *filename, int count)
+ * _close - close file
+ * @file: fd of file tu close
+ * Return: void
+ */
+void _close(int file)
 {
-	char l[count];
-	int fd, rd, cl;
+	int fail;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	fail = close(file);
+	if (fail == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-	rd = read(fd, l, count);
-	if (rd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	close(fd);
-	if (cl == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
 		exit(100);
 	}
-	l[rd] = '\0';
-	return (*l);
-}
-
-/**
-  * copy_file - function that copies the content of a file to another file
-  * @filename:  name of the file to copy
-  * @text_content:  size of the buffer
-  * Return: 1 on success, -1 on failure
-**/
-
-int copy_file(const char *filename, char *text_content)
-{
-	int fd, wr, cl;
-
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
-	wr = write(fd, text_content, _strlen(text_content));
-	if (wr == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
-	cl = close(fd);
-	if (cl == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
-		exit(100);
-	}
-	return (1);
-}
-
-/**
- * _strlen - function that returns the length of a string
- * @s: pointer of type char
- * Return: Length of the string
-**/
-
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-	}
-	return (i);
 }
