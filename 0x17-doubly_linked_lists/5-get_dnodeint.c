96e38ff6559442e059192a12f18c4f5d436089a0
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-03 17:24:57 +0000

    Task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,28 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index -
- *
- * Return: 
+ * get_dnodeint_at_index - function that returns the nth node of a DLL
+ * @head: Head node
+ * @index: index of the node
+ * Return: if the node does not exist, return NULL
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	unsigned int i;
+	dlistint_t *tmp;
 
+	for (i = 0; head != NULL; i++)
+	{
+		if (index == i)
+		{
+			tmp = malloc(sizeof(dlistint_t));
+			if (tmp == NULL)
+				return (NULL);
+			tmp = head;
+			return (tmp);
+		}
+		head = head->next;
+
+	}
+	return (NULL);
 }
