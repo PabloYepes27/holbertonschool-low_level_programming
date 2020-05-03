7e5f6e3d4ffac60f592f31b686df43d5c33c125a
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-03 16:06:49 +0000

    Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,16 @@
 #include "lists.h"
 
 /**
- * dlistint_len -
- *
- * Return: 
+ * dlistint_len - function that returns the number of elements in a DLL
+ * @h: Head node
+ * Return: the number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t i;
+	const dlistint_t *tmp = h;
 
+	for (i = 0; tmp != NULL; i++)
+		tmp = tmp->next;
+	return (i);
 }
