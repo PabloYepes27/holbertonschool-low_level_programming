b216ffd626440f82c2e8ffae5f4a2a9da2f1505a
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-03 22:10:39 +0000

    Task 8 -  V2

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,11 +1,52 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index -
- *
- * Return: 
+ * delete_dnodeint_at_index - delete the node at index index of a DLL
+ * @head: Head node
+ * @index: is the index of the node that should be deleted
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	unsigned int i;
+	dlistint_t *tmp = *head;
 
+	if (head == NULL || *head == NULL || index > dlistint_len(*head))
+		return (-1);
+	for (i = 0; i < index; i++)
+		tmp = tmp->next;
+	if (index == 0)
+	{
+		if (tmp->next == NULL)
+			*head = NULL;
+		else
+		{
+			tmp->next->prev = NULL;
+			*head = tmp->next;
+		}
+	}
+	else if (index == dlistint_len(*head))
+		tmp->prev->next = NULL;
+	else
+	{
+		tmp->prev->next = tmp->next;
+		tmp->next->prev = tmp->prev;
+	}
+	free(tmp);
+	return (1);
+}
+
+
+/**
+ * dlistint_len - function that returns the number of elements in a DLL
+ * @h: Head node
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+	return (i);
 }
