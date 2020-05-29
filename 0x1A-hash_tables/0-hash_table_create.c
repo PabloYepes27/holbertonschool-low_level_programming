31ee5ccfdd2e155a7b08093c66acc7bb674eb6c7
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-29 05:35:22 +0000

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,23 +7,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_hash = NULL;
- 
-    if (size < 1)
-    {
-        return (NULL);
-    }
-    new_hash = malloc(sizeof(hash_table_t));
-    if (!new_hash)
-    {
-        return (NULL);
-    }
-    new_hash->size = size;
-    new_hash->array = calloc(size, sizeof(hash_node_t *));
-    if (!new_hash->array)
-    {
-        free(new_hash);
-        return (NULL);
-    }
-    return (new_hash);
+	hash_table_t *new_hash = NULL;
+
+	if (size < 1)
+	{
+		return (NULL);
+	}
+	new_hash = malloc(sizeof(hash_table_t));
+	if (!new_hash)
+	{
+		return (NULL);
+	}
+	new_hash->size = size;
+	new_hash->array = calloc(size, sizeof(hash_node_t *));
+	if (!new_hash->array)
+	{
+		free(new_hash);
+		return (NULL);
+	}
+	return (new_hash);
 }
