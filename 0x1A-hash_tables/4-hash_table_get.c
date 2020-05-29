bf8ffd3370af0a4d42e185867d9a516c0ecd7671
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-29 05:36:36 +0000

    Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	int index;
 	hash_node_t *ptr;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || *ht == NULL || *key == NULL)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	ptr = ht->array[index];
