c5be0162b97219c9a0a1fa5548cb0a0e17d81f6d
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-29 05:02:48 +0000

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table = NULL;
 
+	if (size < 1)
+		return (NULL);
 	new_hash_table = malloc(sizeof(hash_table_t));
 	if (!new_hash_table)
 		return (NULL);
