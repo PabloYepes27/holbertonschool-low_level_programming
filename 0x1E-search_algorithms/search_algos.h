f7a1c787899cae5979e3c3e6eca40702cb8bee26
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-08-26 12:10:01 -0500

    Task 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int binary_search_rec(int *array, int left, int right, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
