1d02eb441539d28231e86b33da10e112a5f637a0
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-08-27 13:18:37 -0500

    Task 7 condition initial

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,7 +30,7 @@ int jump_search(int *array, size_t size, int value)
 	}
 	printf("Value checked array[%ld] = [%ld]\n", prev, array[prev]);
 	if (array[prev] == value)
-		return (prev);
+		return ((int)prev);
 
 	return (-1);
 }
